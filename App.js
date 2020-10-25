3ee37e62f1005840af707b7d0f54ff82a18e6ab9
bando <louiskaoru@gmail.com>
2020-10-25 23:41:39 +0900

    creating css

--- App.js
+++ App.js
@@ -7,6 +7,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 78,
+  },
+
+  memoList: {
+    width: '100%',
+    flex: 1,
+  },
+  memoListItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 8,
+  },
+  memoDate: {
+    fontSize: 12,
+    color: '#a2a2a2',
   },
 
   appbar: {
@@ -33,26 +52,26 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <View style={styles.appbar}>
-          <Text syle={styles.appbarTitle}>MEMOT</Text>
+          <Text style={styles.appbarTitle}>MEMOT</Text>
         </View>
 
         <View style={styles.memoList}>
-          <View syle={styles.memoListItetm}>
+          <View style={styles.memoListItem}>
             <Text style={styles.memoTitle}>講座のアイテム1</Text>
             <Text style={styles.memoDate}>2017/10/10</Text>
           </View>
 
-          <View syle={styles.memoListItetm}>
+          <View style={styles.memoListItem}>
             <Text style={styles.memoTitle}>講座のアイテム2</Text>
             <Text style={styles.memoDate}>2017/10/10</Text>
           </View>
 
-          <View syle={styles.memoListItetm}>
+          <View style={styles.memoListItem}>
             <Text style={styles.memoTitle}>講座のアイテム3</Text>
             <Text style={styles.memoDate}>2017/10/10</Text>
           </View>
 
-          <View syle={styles.memoListItetm}>
+          <View style={styles.memoListItem}>
             <Text style={styles.memoTitle}>講座のアイテム4</Text>
             <Text style={styles.memoDate}>2017/10/10</Text>
           </View>
